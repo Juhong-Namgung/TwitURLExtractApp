74974d8c24bf801348b10311f35ad280cc8a4e1b
Juhong-Namgung <wnghd9999@naver.com>
2018-11-27 20:57:22 +0900

    Add ValidateURLBolt

--- TweetExtractApp/src/main/java/twitter/main/TweetExtractApp/ValidateURLBolt.java
+++ TweetExtractApp/src/main/java/twitter/main/TweetExtractApp/ValidateURLBolt.java
@@ -1,33 +1,53 @@
 package twitter.main.TweetExtractApp;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Map;
 
 import org.apache.storm.task.OutputCollector;
 import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
 
 public class ValidateURLBolt extends BaseRichBolt {
 
 	OutputCollector collector;
-	
+
 	@Override
 	public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
-		this.collector = collector;		
+		this.collector = collector;
 	}
-	
 
 	@Override
 	public void execute(Tuple input) {
-		
-		
+
+		String expandURL = (String) input.getValueByField("expandurl");
+
+		// Get HTTP Status Code
+		try {
+			URL url = new URL(expandURL);
+			HttpURLConnection http = (HttpURLConnection) url.openConnection();
+			int statusCode = http.getResponseCode();
+
+			if (statusCode >= 400) {
+				System.out.println("#######");
+				System.out.println("URL: " + expandURL + " is not valid!! HTTP Status Code: " + statusCode);
+			} else {
+				collector.emit(new Values(expandURL));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
 	public void declareOutputFields(OutputFieldsDeclarer declarer) {
-		
-		
+		declarer.declare(new Fields("validurl"));
 	}
 
 }
